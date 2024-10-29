fad9720212f2bd8736c158d24ab5de5b477fb9bf
Andrés Molina <andres15mol@gmail.com>
2024-10-29 10:12:51 -0600

    git fix

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,10 +3,10 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2OpError 
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
-from django.db. utils import OperationalError
+from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 
@@ -21,7 +21,6 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         patched_check.assert_called_once_with(databases=['default'])
-
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
