e260eef1ad5af0511248388b871b9b87706d0225
Andrés Molina <andres15mol@gmail.com>
2024-11-03 14:40:08 -0600

    check errors

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,7 +24,7 @@ class ModelTests(TestCase):
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
-            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
             ['test4@example.COM', 'test4@example.com'],
         ]
 
@@ -32,7 +32,7 @@ class ModelTests(TestCase):
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
         
-    def test_new_user_whitout_email_raises_error(self):
+    def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -18,22 +18,21 @@ def create_user(**params):
 
 
 class PublicUserApiTests(TestCase):
-    """"Test the public features of the user API."""
-    
+    """Test the public features of the user API."""
+
     def setUp(self):
         self.client = APIClient()
-    
+
     def test_create_user_success(self):
-        """Test creating  a user is successful."""
+        """Test creating a user is successful."""
         payload = {
             'email': 'test@example.com',
             'password': 'testpass123',
             'name': 'Test Name',
         }
-
         res = self.client.post(CREATE_USER_URL, payload)
 
-        self.assertEqual(res.status_code, status.HTTP_201_CREATESD)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password', res.data)
@@ -47,7 +46,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         create_user(**payload)
-        res= self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -61,7 +60,7 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-        user_exist = get_user_model().objects.filter(
+        user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-        self.assertFalse(user_exist)
+        self.assertFalse(user_exists)
