fd6508526e45684584d07c5b3e481c9b48aa8fc4
Andrés Molina <andres15mol@gmail.com>
2024-12-09 19:43:54 -0600

    Erro detect

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -28,13 +28,13 @@ def detail_url(recipe_id):
     return reverse('recipe:recipe-detail', args=[recipe_id])
 
 def create_recipe(user, **params):
-    """CreATE AND RETURN A SAMPLE RECIPE."""
+    """Create and return a sample recipe."""
     defaults = {
         'title':'Sample recipe title',
         'time_minutes': 22,
         'price': Decimal('5.25'),
         'description': 'Sample description',
-        'link': 'http://example.com/recipe.pdf'
+        'link': 'http://example.com/recipe.pdf',
     }
     defaults.update(params)
 
@@ -42,7 +42,7 @@ def create_recipe(user, **params):
     return recipe
 
 def create_user(**params):
-    """Create an return a new user."""
+    """Create and return a new user."""
     return get_user_model().objects.create_user(**params)
 
 class PublicRecipeAPITests(TestCase):
@@ -67,7 +67,7 @@ class PrivateRecipeApiTests(TestCase):
 
         self.client.force_authenticate(self.user)
     
-    def test_retrive_recipes(self):
+    def test_retrieve_recipes(self):
         """Test retrievingg a list of recipes."""
         create_recipe(user=self.user)
         create_recipe(user=self.user)
@@ -80,7 +80,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.data, serializer.data)
     
     def test_recipe_list_limited_to_user(self):
-        """Test list of recipes is limited to autheticated user."""
+        """Test list of recipes is limited to authenticated user."""
         other_user = create_user(email='other@example.com', password = 'test123')
         create_recipe(user=other_user)
         create_recipe(user=self.user)
@@ -200,7 +200,7 @@ class PrivateRecipeApiTests(TestCase):
             'title': 'Thai Prawn Curry',
             'time_minutes': 30,
             'price':Decimal('2.50'),
-            'tags': [{'name':'Thai'}, {'name': 'Dinner'}]
+            'tags': [{'name':'Thai'}, {'name': 'Dinner'}],
         }
 
         res = self.client.post(RECIPES_URL, payload, format='json')
@@ -240,3 +240,43 @@ class PrivateRecipeApiTests(TestCase):
                 user=self.user,
             ).exists()
             self.assertTrue(exists)
+
+    def test_create_tag_on_update(self):
+            """Test creating tag when updating a recipe."""
+            recipe = create_recipe(user=self.user)
+
+            payload = {'tags': [{'name', 'Lunch'}]}
+            url = detail_url(recipe.id)
+            res = self.client.patch(url, payload, format='json')
+
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            new_tag = Tag.objects.get(user=self.user, name='Lunch')
+            self.assertIn(new_tag, recipe.tags.all())    
+        
+    def test_update_recipe_assign_tag(self):
+            """Test assigning an existing tag when updating a recipe"""
+            tag_breakfast = Tag.objects.create(user=self.user, name='Breakfast')
+            recipe = create_recipe(user=self.user)
+            recipe.tags.add(tag_breakfast)
+
+            tag_lunch = Tag.objects.create(user=self.user, name='Lunch')
+            payload =  {'tags': [{'name': 'Lunch'}]}
+            url = detail_url(recipe.id)
+            res = self.client.patch(url, payload, format='json')
+
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            self.assertIn(tag_lunch, recipe.tags.all())
+            self.assertNotIn(tag_breakfast, recipe.tags.all())    
+        
+    def test_clear_recipe_tags(self):
+            """Test clearing a recipes tags."""
+            tag = Tag.objects.create(user=self.user, name='Dessert')
+            recipe = create_recipe(user=self.user)
+            recipe.tags.add(tag)
+
+            payload = {'tags':[]}
+            url = detail_url(recipe.id)
+            res = self.client.patch(url, payload, format='json')
+
+            self.assertEqual(res.status_code, status.HTTP_200_OK)
+            self.assertEqual(recipe.tags.count(), 0)    
